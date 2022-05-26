49b13e670f7e86cb900f77f3dafca9ea8e6c6579
Lambda Test <lambdatest@Lambdas-MacBook-Pro.local>
2022-05-26 20:48:03 +0530

    added changes

--- codecept.conf.js
+++ codecept.conf.js
@@ -16,7 +16,7 @@ const capabilities = {
     'name': 'Playwright CodeceptJS HyperExecute Test',
     'user': process.env.LT_USERNAME,
     'accessKey': process.env.LT_ACCESS_KEY,
-    'network': true,
+    // 'network': true,
     'video': true,
     'console': true
   }
